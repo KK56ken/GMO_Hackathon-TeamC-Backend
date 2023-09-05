a01883d07960252835da0e07042666d13861fd7a
komekopan2 <yoo.shimizu888@gmail.com>
2023-09-05 17:24:32 +0900

    tmp

--- backend/api/database.py
+++ backend/api/database.py
@@ -2,10 +2,25 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = 'mysql:///localhost:3306'
+# SQLALCHEMY_DATABASE_URL = 'mysql:///localhost:3306'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
-SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+# SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+# Base = declarative_base()
+
+DATABASE = "mysql"
+USER = "user"
+PASSWORD = "password"
+HOST = "172.17.0.1"
+PORT = "3306"
+DB_NAME = "db"
+
+DATABASE_URL = "{}://{}:{}@{}:{}/{}".format(
+    DATABASE, USER, PASSWORD, HOST, PORT, DB_NAME
+)
+print(DATABASE_URL)
+
+Engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=Engine)
+
 Base = declarative_base()
-
-
--- backend/api/main.py
+++ backend/api/main.py
@@ -2,12 +2,12 @@ from fastapi import Depends, FastAPI, HTTPException, Security
 from fastapi.security.api_key import APIKeyHeader, APIKey
 from starlette.status import HTTP_403_FORBIDDEN
 
+import models
 from routers import test
 from util import util
 
-
-from . import schemas, models
-from database import engine, Base, SessionLocal
+import schemas
+from database import Engine, Base, SessionLocal
 from sqlalchemy.orm import Session
 
 correct_key: str = util.get_apikey()
@@ -26,7 +26,7 @@ async def get_api_key(
 app = FastAPI()
 app.include_router(test.router, dependencies=[Depends(get_api_key)], tags=["Test"])
 
-models.Base.metadata.create_all(engine)
+Base.metadata.create_all(Engine)
 
 def get_db():
     db = SessionLocal()
@@ -39,3 +39,18 @@ def get_db():
 def read_root():
     return {"status": "ok"}
 
+@app.post("/users")
+async def create_user(user: schemas.Users, db: Session = Depends(get_db)):
+    db_user = models.User(
+        name=user.name,
+        email=user.email,
+        password=user.password,
+        token=user.token,
+        status=user.status,
+        department_id=user.department_id,
+        slack_id=user.slack_id
+    )
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
