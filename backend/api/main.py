fc8961a2edcf1a99206a0b8a5687a0d7dac55abe
YumaNoguchi39 <n.yuma39@icloud.com>
2023-09-06 10:47:29 +0900

    fixed auth bug

--- backend/api/main.py
+++ backend/api/main.py
@@ -43,7 +43,7 @@ def read_root():
 pwd_cxt = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 @app.post("/auth")
-async def authorization(db: Session = Depends(get_db), request: schemas.User):
+async def authorization(request: schemas.User, db: Session = Depends(get_db)):
     hashdPasswoed = pwd_cxt.hash(request.password)
     new_user = models.User(email=request.email, password=hashdPasswoed)
     db.add(new_user)
