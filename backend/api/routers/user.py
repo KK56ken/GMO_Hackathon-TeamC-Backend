03949b033b084efc1b2e63a8244b36eb59867319
YumaNoguchi39 <n.yuma39@icloud.com>
2023-09-07 23:21:24 +0900

    fixed user profile detail

--- backend/api/routers/user.py
+++ backend/api/routers/user.py
@@ -31,9 +31,11 @@ async def get_profile(id: int, db: Session = Depends(database.get_db), current_u
     temp_tasks = db.query(models.Task.task_id, models.Task.title, models.Task.user_id, models.Task.register_date, models.Task.concern_desc).filter(models.Task.user_id == id).all()
     tasks = []
     for tmp_task in temp_tasks:
-        task_skill_ids = db.query(models.TaskSkill.skill_id).filter(models.TaskSkill.task_id.in_(tmp_task.task_id)).all()
-        task_skill = db.query(models.Skill.skill_name).filter(models.Skill.skill_id.in_(task_skill_ids)).all()
-        task = schemas.Task(task_id=tmp_task.task_id, title=tmp_task.title, user_name=user.name, skill_set=task_skill, task_date=tmp_task.register_date, concern_desc=tmp_task.concern_desc)
+        task_skill_ids = list(db.query(models.TasksSkill.skill_id).filter(models.TasksSkill.task_id == tmp_task.task_id).all())
+        flat_skill_ids = [item[0] for item in task_skill_ids]
+        task_skill = db.query(models.Skill.skill_name).filter(models.Skill.skill_id.in_(flat_skill_ids)).all()
+        flat_task_skill = [item[0] for item in task_skill]
+        task = schemas.Task(task_id=tmp_task.task_id, title=tmp_task.title, user_name=user.name, skill_set=flat_task_skill, task_date=tmp_task.register_date, concern_desc=tmp_task.concern_desc)
         tasks.append(task)
     return {"name": user.name, "department": str(department), "skill_set": skill_set, "slack_id": user.slack_id, "status": user.status, "tasks": tasks}
 
