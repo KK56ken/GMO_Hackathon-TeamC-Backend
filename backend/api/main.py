76d93054ca9331b4fe81dbdf8c2dfb8b01d7d9e1
YumaNoguchi39 <n.yuma39@icloud.com>
2023-09-07 14:05:16 +0900

    fixed cors error

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,12 +1,27 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from typing import List
 from database import Engine, Base
 from routers import authentication, task, user
 
 app = FastAPI()
+
+origins = [
+    'http://localhost:3000',
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
 app.include_router(authentication.router)
 app.include_router(task.router)
 app.include_router(user.router)
+
 
 Base.metadata.create_all(Engine)
 
